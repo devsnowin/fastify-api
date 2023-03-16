f925f316409944a67b1c321222822848d54ac612
devsnowin <snowin.developer@gmail.com>
2023-03-16 14:54:17 +0530

    setuped docker 🐳

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,18 @@ import fastify, { FastifyReply, FastifyRequest } from 'fastify';
 import userRoutes from './modules/user/user.route';
 
 const app = fastify({ logger: true });
-const PORT = process.env.PORT || 3001;
+const PORT = Number(process.env.PORT) || 3001;
 
 app.get('/', (req: FastifyRequest, reply: FastifyReply) => {
-  reply.send({ data: 'hi' });
+  reply.send({ data: 'hello world ✨' });
 });
 
 (async () => {
   app.register(userRoutes, { prefix: 'api/users' });
 
   try {
-    app.listen({ port: PORT as number, host: '0.0.0.0' });
-    console.log(`Listening on http://localhost:${PORT}`);
+    app.listen({ port: PORT, host: '0.0.0.0' });
+    app.log.info(`Listening on http://localhost:${PORT}`);
   } catch (e) {
     app.log.error(e);
     process.exit(1);
